c817795e194700cda0baa1437e0b537355394989
sherramu143 <154246701+sherramu143@users.noreply.github.com>
2025-12-19 14:54:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function daysOfAYear(input){
+if(input>=1 && input <=9999 ){
+	if((input%4==0 && input%100!==0)||(input%400==0)){
+		console.log(366);
+	}
+	else{
+		console.log(365)
+	}
+}
+else{
+	console.log("invLID INPUT")
+}}
+	
+}
