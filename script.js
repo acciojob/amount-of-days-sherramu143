b2045e11ab5a695d44baf3cd103f8275c672d6c7
sherramu143 <154246701+sherramu143@users.noreply.github.com>
2025-12-19 14:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-//your JS code here. If required.
-function daysOfAYear(input){
-if(input>=1 && input <=9999 ){
-	if((input%4==0 && input%100!==0)||(input%400==0)){
-		console.log(366);
-	}
-	else{
-		console.log(365)
-	}
-}
-else{
-	console.log("invLID INPUT")
-}}
-	
+function daysOfAYear(input) {
+  if (input >= 1 && input <= 9999) {
+    // Leap year check
+    if ((input % 4 === 0 && input % 100 !== 0) || (input % 400 === 0)) {
+      console.log(366);
+    } else {
+      console.log(365);
+    }
+  } else {
+    console.log("INVALID INPUT"); // Fixed typo
+  }
 }
